fe8b92e57ff4085300651a34969e70b011a0a228
MuYoungJeong <moo1323@naver.com>
2021-06-20 21:42:47 +0900

    Update few codes

--- js/main.js
+++ js/main.js
@@ -59,8 +59,12 @@
   function setLayout() {
     //각 스크롤 섹션의 높이 세팅
     for (let elem of sceneInfo) {
-      elem.scrollHeight = elem.heightNum * window.innerHeight;
-      elem.objs.container.style.height = `${elem.scrollHeight}px`;
+      if (elem.type === "sticky") {
+        elem.scrollHeight = elem.heightNum * window.innerHeight;
+        elem.objs.container.style.height = `${elem.scrollHeight}px`;
+      } else {
+        elem.scrollHeight = elem.objs.container.offsetHeight;
+      }
     }
 
     yOffset = window.pageYOffset;
@@ -112,24 +116,24 @@
     const values = sceneInfo[currentScene].values;
     const currentYOffset = yOffset - prevScrollHeight;
     const scrollHeight = sceneInfo[currentScene].scrollHeight;
-    const scrollRatio = (yOffset - prevScrollHeight) / scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
 
     console.log(currentYOffset);
 
     switch (currentScene) {
       case 0:
-        const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
-        const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
-        const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
-        const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
-
         if (scrollRatio <= 0.22) {
           // in
-          objs.messageA.style.opacity = messageA_opacity_in;
+          objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
+          const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
           objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
         } else {
           // out
-          objs.messageA.style.opacity = messageA_opacity_out;
+          objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
+          const messageA_translateY_out = calcValues(
+            values.messageA_translateY_out,
+            currentYOffset,
+          );
           objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
         }
 
